35ba67e1869bb640bc0a35f6f30c5d6d4f71195f
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-05-01 14:18:22 -0300

    fix: converted product list to table

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,17 +19,35 @@ function Home() {
     }, []);
 
     return (
-        <div className="container mx-auto mt-4">
+        <div className="container flex flex-col justify-center items-center mt-4">
             <h2 className="text-2xl font-bold mb-4">Products</h2>
-            <ul>
-                {products.map(product => (
-                    <li key={product._id} className="mb-2">
-                        <Link to={`/product/${product._id}`} className="text-blue-500 hover:underline">
-                            <span className="font-bold">{product.productName}</span> - {product.productQuantity}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <table className="table-auto">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2">Produto</th>
+                        <th className="px-4 py-2">Categoria</th>
+                        <th className="px-4 py-2">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map(product => (
+                        <tr key={product._id} className="border-b border-gray-200">
+                            <td className="px-4 py-2">
+                                <Link to={`/product/${product._id}`} className="text-blue-500 hover:underline font-bold">
+                                    {product.productName}
+                                </Link>
+                            </td>
+                            <td className="px-4 py-2">
+                                {product.productCategory}
+                            </td>
+                            <td className="flex gap-2 text-center px-4 py-2">
+                                <p className="text-red-600">X</p>
+                                <p className="text-blue-600">Editar</p>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
