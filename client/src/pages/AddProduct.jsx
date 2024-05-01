bf7ff307e27c8189f5e62769750b983b3af827b9
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-05-01 13:13:29 -0300

    fix: changed quantity to category and added axios baseconfig

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -1,20 +1,22 @@
-import axios from "axios";
 import { useState } from "react";
+import API from "../services/axios-config";
 
 function AddProduct() {
     const [productName, setProductName] = useState('');
-    const [productQuantity, setProductQuantity] = useState('');
+    const [productCategory, setProductCategory] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post('http://localhost:5000/api/products', {
+            await API.post('/products', {
                 productName,
-                productQuantity
+                productCategory
             });
+
             alert('Product added successfully!');
+
             setProductName('');
-            setProductQuantity('');
+            setProductCategory('');
         } catch (error) {
             console.error('Error adding product:', error);
         }
@@ -25,23 +27,25 @@ function AddProduct() {
             <h2 className="text-2xl font-bold mb-4">Add Product</h2>
             <form onSubmit={handleSubmit}>
                 <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" htmlFor="productName">Product Name:</label>
+                    <label className="block text-sm font-bold mb-2" htmlFor="productName">Nome do Produto:</label>
                     <input
                         type="text"
                         id="productName"
                         className="border border-gray-300 rounded p-2 w-full"
                         value={productName}
+                        placeholder="digite o nome do produto..."
                         onChange={(e) => setProductName(e.target.value)}
                     />
                 </div>
                 <div className="mb-4">
-                    <label className="block text-sm font-bold mb-2" htmlFor="productQuantity">Product Quantity:</label>
+                    <label className="block text-sm font-bold mb-2" htmlFor="productCategory">Categoria:</label>
                     <input
-                        type="number"
-                        id="productQuantity"
+                        type="text"
+                        id="productCategory"
                         className="border border-gray-300 rounded p-2 w-full"
-                        value={productQuantity}
-                        onChange={(e) => setProductQuantity(e.target.value)}
+                        placeholder="digite a categoria d produto..."
+                        value={productCategory}
+                        onChange={(e) => setProductCategory(e.target.value)}
                     />
                 </div>
                 <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">Add Product</button>
