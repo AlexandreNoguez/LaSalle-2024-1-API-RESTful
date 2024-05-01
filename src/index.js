7a9e2cfd85f5e944007e8fe5d7735a9347b9a17f
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-04-30 21:14:37 -0300

    build: created project LaSalle API RESTful

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+    console.log("Server started at" + new Date());
+})
