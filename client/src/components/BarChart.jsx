e571e48772e63ec5229cdf61afa01d702f2e134d
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-05-02 20:00:27 -0300

    feat: added barchart

---
+++ client/src/components/BarChart.jsx
@@ -0,0 +1,62 @@
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import PropTypes from 'prop-types';
+
+BarChart.propTypes = {
+    dataChart: PropTypes.shape({
+        categoryNames: PropTypes.arrayOf(PropTypes.string),
+        quantities: PropTypes.arrayOf(PropTypes.number),
+    }),
+};
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+function BarChart({ dataChart }) {
+
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Chart.js Bar Chart',
+            },
+        },
+    };
+
+    const data = {
+        labels: dataChart?.categoryNames,
+        datasets: [
+            {
+                label: 'Dataset 1',
+                data: dataChart?.quantities,
+                backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            },
+        ],
+    };
+
+    return (
+        <div>
+            <Bar options={options} data={data} />
+        </div>
+    )
+}
+
+export default BarChart;
