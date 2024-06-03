ef9b1d584d60fd611512dab5f7fb8eb772f2c427
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2024-06-02 23:34:57 -0300

    fix: added cluster to the string connection

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
 const mongoose = require('mongoose')
 
 const DB_USER = process.env.DB_USER
 const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_CLUSTER = process.env.DB_PASSWORD
+const DB_CLUSTER = process.env.DB_CLUSTER
 
 mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.zspqfad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
     .then(() => {
